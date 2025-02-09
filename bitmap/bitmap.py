199941248b1fa67f36a8f65aca9671e74740088e
shivank <shivang.arya09@gmail.com>
2025-02-09 13:35:37 +0530

    bitmap finished

---
+++ bitmap/bitmap.py
@@ -0,0 +1,21 @@
+import sys
+from text import text
+
+def main():
+    prompt = input("Enter a Word") 
+    if prompt == "":
+        sys.exit("error")
+    bitmap = text() # patterns
+    for line in bitmap.splitlines():
+        for i, bit in enumerate(line): # iterating over the line and each bit to replace message
+            if bit == ' ':
+                print(" ", end='')
+            else:
+                print(prompt[i % len(prompt)], end='') # entering char from the prompt replacing bits.
+        print()
+
+
+
+
+if __name__ == "__main__":
+    main()
