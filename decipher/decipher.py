0108c9cec252acef9c7223cff7cdac2963006c04
shivank <shivang.arya09@gmail.com>
2025-02-11 17:42:35 +0530

    decipher finished

---
+++ decipher/decipher.py
@@ -0,0 +1,29 @@
+Symbols = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P",
+           "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+def decipher(text):
+    text = text.upper() 
+    for key in range(1, 27): 
+        decrypted = []
+        for i in text:
+            if i == " ":
+                decrypted.append(" ")
+            elif i in Symbols:
+                position = Symbols.index(i)
+                new_position = (position + key) % 26  # wrap around
+                decrypted.append(Symbols[new_position])
+            else:
+                decrypted.append(i)
+
+        print(f"Key #{key}: {''.join(decrypted)}")  # each key
+
+def main():
+    try:
+        while True:
+            text = input("Enter the encrypted caesar cipher to crack\n>>> ")
+            decipher(text)
+    except Exception as e:
+        print(e)
+
+if __name__ == "__main__":
+    main()
