8e3be98c1e85aab46f245cd5383aca95cbabe318
shivank0927 <shivang.arya09@gmail.com>
2025-02-13 23:14:12 +0530

    cho-han

---
+++ cho-han/cho-han.py
@@ -0,0 +1,24 @@
+import random
+import sys
+
+
+def main():
+    money = 10000
+    while True:
+        bet = getbet()
+        if 0 >= bet >= 10000:
+            print("error in bet")
+        elif bet == ".quit":
+            sys.exit(".quitted")
+
+        pass
+
+
+def getbet():
+
+    bet = int(input(f"Place your bet or .quit to exit the game\n>>>"))
+    return bet
+
+
+if __name__ == "__main__":
+    main()
