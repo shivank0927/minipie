4bb1ea8b0bb13574b04439352ba8cbef2c4f4feb
shivank0927 <shivang.arya09@gmail.com>
2025-02-13 22:06:52 +0530

    new

--- carrot/carrot.py
+++ carrot/carrot.py
@@ -1,5 +1,31 @@
 import sys
+import random
+
 print("""Welcome, It's a two player game enter your names\n
       Rules:\n""")
 def main():
-    p1 = input("Enter your name\n")
+        p1 = input("Enter your name, player 1\n>>>").title().strip()
+        p2 = input("Enter your name, player 2\n>>>").title().strip()
+
+        print("""There are two boxes, one is gold and the other is lead\n
+              choose one box out of the two, the person who gets to choose will\n
+              be random. Good luck!""")
+        
+        while True:
+            
+            choose = random.randint(1, 2)
+            while True:
+                if choose == 1:
+                    print(f"{p1} gets the chance to choose\nType which box do you wanna choose\n")
+                    box = input()
+                    if box == "gold":
+                            pass
+                    elif box == "lead":
+                            pass
+                    else:
+                            print("Incorrect choice")
+
+                    
+
+
+
