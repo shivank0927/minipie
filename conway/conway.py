c17319703f7110620cce96618e33fbc4655193ff
shivank0927 <shivang.arya09@gmail.com>
2025-02-23 20:29:18 +0530

    conway finished

--- conway/conway.py
+++ conway/conway.py
@@ -3,29 +3,82 @@ import random
 import sys
 import time
 
-
-WIDTH = 79
-HEIGHT = 20
-
-ALIVE = '.' # alive and dead cells
-DEAD = '1' 
-
-# the cells, putting them in (x, y) tuple depending upon random lib.
-
-nextCells = {}
-# adding random dead / alive cells 
-for x in range(WIDTH):
-    for y in range(HEIGHT):
-        if random.randint(0, 1) == 0:
-            nextCells[(x, y)] = ALIVE
-        else:
-            nextCells[(x, y)] = DEAD
-
-while True:
+try:
+    width = int(input("Enter the width of cell for simulation\n>>> "))
+    height = int(input("Enter the height of cell for simulation\n>>> "))
+    cell = str(input("Enter a character to represent alive cell ( dead cell will be blank! )\n>>> "))
     
-    print("\n" * 50)
-    cells = copy.deepcopy(nextCells) # create deep copy of object
+except ValueError:
+    print("Wrong value entered")
 
-    for y in range(HEIGHT):
-        for x in range(WIDTH):
-            pass
+alive = f'{cell}' # alive and dead cells
+dead = ' '
+
+# adding random dead / alive cells
+def initialize(width, height, alive, dead):
+    grid = {}
+    for x in range(width):
+        for y in range(height):
+            if random.randint(0, 1) == 0:
+                grid[(x, y)] = alive
+            else:
+                grid[(x, y)] = dead
+    return grid
+
+# display the grid
+def display(grid, width, height, simulation):
+    print("\n" * 50)
+    for y in range(height):
+        for x in range(width):
+            print(grid[(x, y)], end='')
+        print()
+    print(f"Simulation: {simulation}")
+    print("press Ctrl + C to exit.")
+
+# count number of living neighbours
+def count_neighbors(grid, x, y, width, height, alive):
+    neighbors = [
+        ((x - 1) % width, (y - 1) % height),
+        (x, (y - 1) % height),
+        ((x + 1) % width, (y - 1) % height),
+        ((x - 1) % width, y),
+        ((x + 1) % width, y),
+        ((x - 1) % width, (y + 1) % height),
+        (x, (y + 1) % height),
+        ((x + 1) % width, (y + 1) % height),
+    ] # initializing every possible cell pos which is either alive or dead
+    count = 0
+    for nx, ny in neighbors:
+        if grid[(nx, ny)] == alive:
+            count += 1
+    return count
+
+# setup of cells
+def update(grid, width, height, alive, dead):
+    new_grid = copy.deepcopy(grid)
+    for x in range(width):
+        for y in range(height):
+            neighbors = count_neighbors(grid, x, y, width, height, alive)
+            if grid[(x, y)] == alive and (neighbors == 2 or neighbors == 3):
+                new_grid[(x, y)] = alive
+            elif grid[(x, y)] == dead and neighbors == 3:
+                new_grid[(x, y)] = alive
+            else:
+                new_grid[(x, y)] = dead
+    return new_grid
+
+def main():
+    grid = initialize(width, height, alive, dead)
+    simulation = 1
+    
+    while True:
+        display(grid, width, height, simulation)
+        grid = update(grid, width, height, alive, dead)
+        simulation += 1
+        try:
+            time.sleep(2)
+        except KeyboardInterrupt:
+            sys.exit("stopped!")
+
+if __name__ == "__main__":
+    main()
